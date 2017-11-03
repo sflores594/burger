9fecf49812e148a5adda1983a3234907e8cca972
StephanieFlores <sflores594.edu@gmail.com>
2017-11-02 20:49:39 -0500

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE burgers_db;
+
+-- Makes it so all of the following code will affect favorite_db --
+USE burgers_db;
+
+-- Creates the table "favorite_foods" within favorite_db --
+CREATE TABLE burgerss (
+  -- Creates a numeric column called "id" which automatically increments and cannot be null --
+  id INTEGER(11) AUTO_INCREMENT PRIMARY KEY,
+  burger_name VARCHAR(50) NOT NULL,
+ devoured BOOLEAN DEFAULT false,
+ date TIMESTAMP NOT NULL,
+  PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jalapeno Bucket Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado Bean Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Bamazon Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Habanero Burger');
